363625dc678cc4d480c1c3ff9bcbc01f0d59e7cd
Agnesia Peronika Lumban Raja <47594293+AgnesiaPeronika@users.noreply.github.com>
2019-04-23 20:04:09 +0700

    Add files via upload

---
+++ application/views/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="kost.css">
+	<title>
+		InfoKost
+	</title>
+
+</head>
+
+<body>
+	<header id="home">
+        <nav id="nav-wrap">
+         <ul id="nav" class="nav">
+            <li class="current"><a class="smoothscroll" href=#home>Home</a></li>
+            <li><a class="smoothscroll" href=#profil>Profile</a></li>
+	        <li><a class="smoothscroll" href=#resume>Resume</a></li>
+            <li><a class="smoothscroll" href=#contact>Contact</a></li>
+         </ul> 
+      </nav>
+
+      <div class="banner">
+         <div class="banner-text">
+            <h3>Hi there! </h3>
+            <h1 class="responsive-headline">Welcome To Info Kost</h1>
+         </div>
+      </div>
+        
+    </header>
+    
+    
+    <section id="profil">
+        <h1><span>ABOUT KOST</span></h1>
+
+        <div class="pict">
+            <img class="profile" src="img/Profile.JPG" alt="">
+         </div>
+         <div class="isi">
+
+            <p>
+            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Mau cari kos kosan ? Cari di ... :)
+            </p>
+                <div class="baris">
+                    <ul>
+                        <li>
+                            <font size="5px">InfoKost</font><br>
+                            <img class="pic1" src="pic2.jpg">
+                            <img class="pic2" src="pic3.jpg">
+                        </li>                        
+                    </ul>
+                </div>
+            </div>
+         </div>
+            </div>
+                  
+     </section>
+
+     <section id="resume">
+        <h1><span>ABOUT KOST</span></h1>
+        <div class="kolom">
+                <div class="baris">
+                <div class="title_resume">InfoKost - Aplikasi Pencari Kos kosan :) </div>
+                <div class="isi_resume">
+                    <ul>
+                        <li>
+                            <font size="5px">InfoKos menyajikan informasi Kamar kosan, lengkap dengan fasilitas kost, harga kost, dan dekorasi kamar beserta foto desain kamar yang disesuaikan dengan kondisi sebenarnya. Setiap Rumah Kost yang ada di Infokos benar-benar kami datangi, kami verifikasi, kami seleksi dan kami ambil data langsung, termasuk kost yang didaftarkan oleh pemilik atau umum. Informasi ketersediaan kamar kost dan harga kost kami edit untuk memastikan info kost kami akurat dan bermanfaat untuk anak kost. Di InfoKos kini juga telah ditambahkan berbagai info kosan dengan harga murah. </font><br>
+                        </li><br>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <div class="baris">
+                <div class="title_resume">Berbagai Informasi akurat yang dapat kamu lihat di InfoKost :) </div>
+                <div class="isi_resume">
+                    <ul>
+                        <li>
+                            <font size="5px">Foto-foto lengkap fasilitas kosan</font><br>
+                        </li><br>
+                        <li>
+                            <font size="5px">Filter pencarian kost untuk membantumu menemukan kosan idamanmu</font><br>
+                        </li><br>
+                        <li>
+                            <font size="5px">Harga Kost yang selalu diedit</font><br>
+                        </li><br>
+                        <li>
+                            <font size="5px">Informasi ketersediaan kamar kost yang selalu diedit</font><br>
+                        </li><br>
+                        <li>
+                            <font size="5px">Foto-foto lengkap desain kamar kost</font><br>
+                        </li><br>                        
+                    </ul>
+                </div>
+            </div>
+        </div>    
+    </section>
+
+    <section id="contact">
+        <h1><span class="judul_section">CONTACT</span></h1>
+        <h4>Address and Phone Number</h4>
+        <p>
+            Info Kost<br>
+            Jawa Barat, Bandung Selatan<br>
+            (+62) 85262650962
+        </p>
+        <br><h4>Social Media</h4>
+        <p>
+            WhatsApp : 085262650962<br>
+            Instagram : <a href="https://instagram.com/Info_Kost" style="color : #838C95">Info_Kost</a><br>
+            Line &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : InfoKost_29<br>
+            Facebook : Info Kost<br>
+        </p>
+    </section>
+
+</body>
+
+<!-- <script src="https://www.gstatic.com/firebasejs/5.8.4/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBVd4OcqJY6NcLlIJtXTytlO7Y15nr7XN0",
+    authDomain: "cv-webpro-6aa38.firebaseapp.com",
+    databaseURL: "https://cv-webpro-6aa38.firebaseio.com",
+    projectId: "cv-webpro-6aa38",
+    storageBucket: "cv-webpro-6aa38.appspot.com",
+    messagingSenderId: "151453043794"
+  };
+  firebase.initializeApp(config);
+</script> -->
+</html>
