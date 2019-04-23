5392165ef634713abae64be6ba50685af63df71a
Dewi-Annisa <47683285+Dewi-Annisa@users.noreply.github.com>
2019-04-23 23:59:33 +0700

    Update profil.php

--- application/views/profil.php
+++ application/views/profil.php
@@ -7,32 +7,32 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/Profile.css')?>">
 </head>
 <body>
+<body style="background-image: url(assets/image/prof.jpg)"></body>
 	<div class="container">
-    <div>
+	<div>
       <h2 class="heading">Profil</h2>
     </div>
     <form>
+      <h3>Nama</h3>
+      <p>Dinda Almirah</p>
+      <div class="clear"></div>
+      <h3>Gender</h3>
+      <p>Perempuan</p>
+      <div class="clear"></div>
+      <h3>Alamat</h3>
+      <p>Jl. Dr. Soetomo No. 67, Surabaya</p>
+      <div class="clear"></div>
       <div class="row">
         <div id="img">
-          <img src="assets/image/email.png" margin-right="100px" width="30" height="20">
+          <img src="assets/image/telpon.png" margin-right="100px" width="30" height="25">
         </div>
-          <h3>Email</h3>
-          <p>Belum verifikasi</p>
-        <div class="row">
-          <input type="submit" value="Verifikasi">
-        </div>
-      <div class="clear"></div>
+      <h3>Phone</h3>
+      <p>Belum verifikasi</p>
+      <div class="row">
+      <a href="application/views/editPROFILE.php">
+        <input type="submit" value="Edit">
+      </a>
     </div>
-    <div class="row">
-      <div id="img">
-        <img src="assets/image/telpon.png" margin-right="100px" width="30" height="25">
-      </div>
-        <h3>Phone</h3>
-        <p>Belum verifikasi</p>
-        <div class="row">
-          <input type="submit" value="Verifikasi">
-          <a target="_blank" href="">
-        </div>
     </div>
   </form>
 </div>
