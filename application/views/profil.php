680bc638fe046c4cfcf0f3f095543bbb3851dc77
Dewi-Annisa <47683285+Dewi-Annisa@users.noreply.github.com>
2019-04-28 20:49:01 +0700

    Update profil.php

--- application/views/profil.php
+++ application/views/profil.php
@@ -1,40 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Profil Pengguna - InfoKost</title>
+	<title>Profil Penjaga - InfoKost</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta charset="UTF-8">
+	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/Profile.css')?>">
 </head>
 <body>
-<body style="background-image: url(assets/image/prof.jpg)"></body>
+	<body style="background-image: url(assets/image/prof.jpg)"></body>
 	<div class="container">
-	<div>
-      <h2 class="heading">Profil</h2>
-    </div>
-    <form>
-      <h3>Nama</h3>
-      <p>Dinda Almirah</p>
-      <div class="clear"></div>
-      <h3>Gender</h3>
-      <p>Perempuan</p>
-      <div class="clear"></div>
-      <h3>Alamat</h3>
-      <p>Jl. Dr. Soetomo No. 67, Surabaya</p>
-      <div class="clear"></div>
-      <div class="row">
-        <div id="img">
-          <img src="assets/image/telpon.png" margin-right="100px" width="30" height="25">
-        </div>
-      <h3>Phone</h3>
-      <p>Belum verifikasi</p>
-      <div class="row">
-      <a href="application/views/editPROFILE.php">
-        <input type="submit" value="Edit">
-      </a>
-    </div>
-    </div>
-  </form>
-</div>
+		<div>
+      		<h2 class="heading">Profil</h2>
+    		</div>
+    		<form>
+      			<h3>Nama</h3>
+      			<p>Dinda Almirah</p>
+      			<div class="clear"></div>
+      			<h3>Gender</h3>
+      			<p>Perempuan</p>
+      			<div class="clear"></div>
+      			<h3>Alamat</h3>
+      			<p>Jl. Dr. Soetomo No. 67, Surabaya</p>
+      			<div class="clear"></div>
+      			<div class="row">
+        			<div id="img">
+          				<img src="assets/image/telpon.png" margin-right="100px" width="30" height="25">
+      				</div>
+      			<h3>Phone</h3>
+      			<p>081567345000</p>
+      			<div class="row">
+      				<a href="application/views/editPROFILE.php">
+        				<input type="submit" value="Edit">
+      				</a>
+    			</div>
+   			</div>
+  		</form>
+	</div>
 </body>
 </html>
