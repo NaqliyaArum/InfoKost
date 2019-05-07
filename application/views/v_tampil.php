084f7db6c4f797a9b24e6ef6dd2c449b4519d07f
NaqliyaArum <47947256+NaqliyaArum@users.noreply.github.com>
2019-05-07 18:24:14 +0700

    Update v_tampil.php

--- application/views/v_tampil.php
+++ application/views/v_tampil.php
@@ -1,64 +1,61 @@
-<?php
-include "config.php";
-$query = mysqli_query($connection,"SELECT * FROM my_kost");
-?>)
+<div class="container">
+    <?php if ($this->session->flashdata('flash')) : ?>
+    <div class="row mt-3">
+        <div class="col-md-6">
+            <div class="alert alert-success alert-dismissible fade show" role="alert" style="margin-top : 50px">
+                Data Kost <strong>berhasil</strong> <?= $this->session->flashdata('flash'); ?>.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+    </div>
+    <?php endif; ?>
+    <div class="row mt-3">
+        <div class="col md-6">
+            <form action="<?php echo base_url('index.php/Welcome/admin');?>" method="post">
+                <div class="input-group" style="margin-top : 50px">
+                    <input type="text" class="form-control" placeholder="Cari data kost ... " name="keyword" style="margin-top :50px">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="submit" style="margin-top : 50px">Cari</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 
-<head>
-	<title>Tampil</title>
-</head>
-<body>
-	<form action="" method=""></form>
-	<center><h1>Tampilan Data</h1></center>
-	<center><?php echo anchor('crud/tambah','Tambah Data'); ?>
-	<table style="margin:20px auto;" border="1">
-		<tr>
-			<th>Jenis kosan</th>
-			<th>Nama</th>
-			<th>Lokasi</th>
-			<th>Fasilitas Kamar</th>
-			<th>Luas Kamar</th>
-			<th>Fasilitas Parkir</th>
-			<th>Akses Lingkungan</th>
-			<th>Durasi Penyewaan Kost</th>
-			<th>Keterangan biaya sewa</th>
-			<th>keterangan biaya lain</th>
-			<th>deskripsi kost</th>
-			<th>nomor hp</th>
-			<th>action</th>
-		</tr>
-		<!-- <?php if(mysqli_num_rows($query)>0){ ?> -->
-		
-		<?php
-			$no = 1;
-			while($data = mysqli_fetch_array($query)){
-		?>
-		<tr>
-			<td><?php echo $no ?></td>
-			<td><?php echo $data['jk_input'];?></td>
-			<td><?php echo $data['name_input'];?></td>
-			<td><?php echo $data['loc_input'];?></td>
-			<td><?php echo $data['fk_input'];?></td>
-			<td><?php echo $data['Luas_input'];?></td>
-			<td><?php echo $data['mnd_input'];?></td>
-			<td><?php echo $data['fp_input'];?></td>
-			<td><?php echo $data['al_input'];?></td>
-			<td><?php echo $data['dpk_input'];?></td>
-			<td><?php echo $data['kbs_input'];?></td>
-			<td><?php echo $data['kl_input'];?></td>
-			<td><?php echo $data['kb_input'];?></td>
-			<td><?php echo $data['dk_input'];?></td>
-			<td><?php echo $data['nohp_input'];?></td>
-			<td>
-				<a href="#">Delete</a> |
-                <a href="#">Update</a>
-			</td>
-		</tr>
-		<?php $no++; } ?>
-	<?php } ?>
-</table>
-</form>
-</body>
+    <div class="row mt-5">
+        <div class="col">
+            <h3 class="text-center">Daftar Kost</h3>
+            <?php if (empty($kost)) : ?>
+            <div class="alert alert-danger" role="alert">
+                Data tidak ditemukan
+            </div>
+            <?php endif; ?>
 
-
-			
-			
+            <table class="table mt-5">
+                <thead>
+                    <tr>
+                        <th class="text-center" scope="col">Jenis Kostan</th>
+                        <th class="text-center" scope="col">Nama</th>
+                        <th class="text-center" scope="col">Lokasi</th>
+                        <th class="text-center" scope="col">Fasilitas Kamar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><?php foreach ($kost as $k) : ?>
+                        <td class="text-center"><?= $k['jk_input']; ?></td>
+                        <td class="text-center"><?= $k['name_input']; ?></td>
+                        <td class="text-center"><?= $k['loc_input']; ?></td>
+                        <td class="text-center"><?= $k['fk_input']; ?></td>
+                        <td class="text-center">
+<!--                          <a href="<?= base_url(); ?>index.php/Welcome/hapus/<?= $k['id_kost'] ?>" class="badge badge-danger float-center" onclick="return confirm('Apakah anda yakin menghapus data ini?');" ?>hapus</a>
+                            <a href="<?= base_url(); ?>index.php/Welcome/ubah/<?= $k['id_kost'] ?>" class="badge badge-success float-center" ?>ubah</a> -->
+                        </td>
+                    </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div> 
