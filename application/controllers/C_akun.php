69b5b39d0db5ffac4f30fc4ccb0e2790487c3268
Dewi-Annisa <47683285+Dewi-Annisa@users.noreply.github.com>
2019-05-07 20:06:31 +0700

    Update C_akun.php

--- application/controllers/C_akun.php
+++ application/controllers/C_akun.php
@@ -5,4 +5,34 @@ class C_akun extends CI_Controller{
 		$data['user'] = $this->insert->editAkun()->result();
 		$this->load->view('profil',$data);
 	}
+	
+	public function ubahDataUser($id)
+	{
+		$data['judul'] = 'Edit Profil';
+
+		$this->form_validation->set_rules('First','First','required');
+		$this->form_validation->set_rules('Last','Last','required');
+		$this->form_validation->set_rules('gender','gender','required');
+		$this->form_validation->set_rules('Alamat','Alamat','required');
+		$this->form_validation->set_rules('Handphone','Handphone','required');
+		$this->form_validation->set_rules('Password','Password','required');
+
+		if ($this->form_validation->run() == false){
+			$data['user'] = $this->M_akun-> getUserById($id);
+			$this->load->view('template/header', $data);
+			$this->load->view('editPROFILE');
+			$this->load->view('template/footer', $data);	
+		
+		}else{
+			$this->db->where('id', $id);
+			$this->M_akun->ubahDataUser($id);
+			if($this->db->affected_rows()){
+				$this->session->set_flashdata('flash', 'data diubah');
+				redirect('C_akun');
+			}else{
+				$this->session->set_flashdata('flash', 'data change failed');
+				redirect('C_akun');
+			}		
+		}
+	}
 }
