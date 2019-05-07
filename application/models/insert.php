f545828d6d2cca67d38cf3b6575809efedde6661
shintaayutyas13 <47978098+shintaayutyas13@users.noreply.github.com>
2019-05-07 19:39:04 +0700

    Update insert.php

--- application/models/insert.php
+++ application/models/insert.php
@@ -57,4 +57,26 @@ class insert extends CI_Model {
 		$this->db->delete('datakost',array('id_kost'=>$id));
 	}
 	
+	public function ubahdatakost($id)
+	{
+		$data = [
+			"jk_input" =>$this->input->post('jk_input', true),
+			"name_input" =>$this->input->post('name_input', true),
+			"loc_input" =>$this->input->post('loc_input', true),
+			"fk_input" =>$this->input->post('fk_input', true),
+			"Luas_input" =>$this->input->post('Luas_input', true),
+			"mnd_input" =>$this->input->post('mnd_input', true),
+			"fp_input" =>$this->input->post('fp_input', true),
+			"al_input" =>$this->input->post('al_input', true),	
+			"dpk_input" =>$this->input->post('dpk_input', true),
+			"kbs_input" =>$this->input->post('kbs_input', true),
+			"kl_input" =>$this->input->post('kl_input', true),
+			"kb_input" =>$this->input->post('kb_input', true),
+			"dk_input" =>$this->input->post('dk_input', true),
+			"nohp_input" =>$this->input->post('nohp_input', true),
+			//"image_kost" =>$this->input->post('image_kost'),
+		];
+		$this->db->update('datakost', $data);	
+	}
+	
 }
