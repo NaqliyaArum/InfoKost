e36913c94de80ab80d115ad479d53ea8cdf7cd3d
Dewi-Annisa <47683285+Dewi-Annisa@users.noreply.github.com>
2019-05-07 21:47:45 +0700

    Update C_akun.php

--- application/controllers/C_akun.php
+++ application/controllers/C_akun.php
@@ -2,6 +2,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class C_akun extends CI_Controller{
+	public function __construct()
+	{
+		parent::__construct();
+        	$this->load->model("M_akun");
+        	$this->load->library('form_validation');
+    	}
+	
 	public function index()
 	{
 		$data['user'] = $this->insert->editAkun()->result();
