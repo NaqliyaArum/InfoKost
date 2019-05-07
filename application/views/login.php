8857d21142cd6dbd68c8c7defd572a447cd8a6ad
NaqliyaArum <47947256+NaqliyaArum@users.noreply.github.com>
2019-05-07 18:18:55 +0700

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -9,12 +9,14 @@
   <img class="pic1" src="<?php echo base_url('assets/image/iklan2.jpg');?>">
   <div class="lg">
       <h1> Login </h1>
-      <form action="insert.php" method="POST">
+      <form action="<?php echo base_url('/index.php/Welcome/proses_login');?>" method="POST">
         <p>No Handphone</p>
-        <input type="text"  name=""  placeholder="No Handphone"><br><br>
+        <input type="text"  name="No"  placeholder="No Handphone"><br><br>
+        <span><?php echo form_error('No')?></span>
         <p>Password</p>
-        <input type="password"  name=""  placeholder="Password"><br><br>
+        <input type="password"  name="Pass"  placeholder="Password"><br><br>
         <br><br>
+        <span><?php echo form_error('Pass')?></span>
         <button type="submit">Login</button>
         <h3>Don't have account ? <a class="aclk" href="<?php echo base_url('index.php/Welcome/register/');?>">Click here!</a></h3>
       </form>
